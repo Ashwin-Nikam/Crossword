03025b8a1513da71c74b5b8434c3a805af9e2e4a
Ashwin-Nikam <nikam.ashwin7@gmail.com>
2018-05-08 00:23:16 -0400

    Updated model in book

--- models/book.js
+++ models/book.js
@@ -48,3 +48,18 @@ module.exports.getBookById = function(id, callback) {
 module.exports.addBook = function(book, callback) {
 	Book.create(book, callback);
 }
+
+// Update Book
+module.exports.updateBook = function(id, book, options, callback) {
+	var query = {_id: id};
+	var update = {
+		title: book.title,
+		genre: book.genre,
+		description: book.description,
+		author: book.author,
+		publisher: book.publisher,
+		pages: book.pages,
+		buy_url: book.buy_url
+	}
+	Book.findOneAndUpdate(query, update, options, callback);
+}
